acbc124fabb37decc4b7c95dfc3926c8d54a4746
sunday-robot <sgx03150@nifty.com>
2020-03-17 18:34:49 +0900

    a

--- libWordVecSample/Program.cs
+++ libWordVecSample/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using wordVec;
 
 namespace libWordVecSample
@@ -8,11 +10,54 @@ namespace libWordVecSample
     {
         static void Main(string[] args)
         {
-            var wv = WordVecLoader.Load(args[0]);
+            //EvaluateSample(args[0]);
+            //SerializeSample(args[0]);
+            DeserializaSample();
+        }
+
+        static void SerializeSample(string tsvFilePath)
+        {
+            var binFilePath = "ja.bin";
+            var bf = new BinaryFormatter();
+
+            var wv = WordVecLoader.Load(tsvFilePath);
+            WordVec wv2;
+
+            {
+                var fs = new FileStream(binFilePath, FileMode.Create);
+                bf.Serialize(fs, wv);
+                fs.Close();
+            }
+            {
+                var fs = new FileStream(binFilePath, FileMode.Open);
+                wv2 = (WordVec)bf.Deserialize(fs);
+                fs.Close();
+            }
+            var result = wv2.Evaluate("こんにちは + 世界");
+            result.Print();
+        }
+
+        static void DeserializaSample()
+        {
+            var binFilePath = "../../../ja.bin";
+            var bf = new BinaryFormatter();
+
+            var fs = new FileStream(binFilePath, FileMode.Open);
+            var wv = (WordVec)bf.Deserialize(fs);
+            fs.Close();
+
+            var result = wv.Evaluate("こんにちは + 世界");
+            result.Print();
+        }
+
+        static void EvaluateSample(string filePath)
+        {
+            var wv = WordVecLoader.Load(filePath);
             //wv.Print();
             var s = " こんにちは   + 世界 ";
             var result = wv.Evaluate(s);
             result.Print();
         }
+
     }
 }
